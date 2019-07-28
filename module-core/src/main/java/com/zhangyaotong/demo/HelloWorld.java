6399531f591eab9523fa29a71018620c46016bc8
Yaotong Zhang <yaotongzhang1992@gmail.com>
2019-07-28 15:24:26 +0800

    hello,world

---
+++ module-core/src/main/java/com/zhangyaotong/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.zhangyaotong.demo;
+
+/**
+ * @author ZhangYaotong
+ * @create 2019-07-28 15:23
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
